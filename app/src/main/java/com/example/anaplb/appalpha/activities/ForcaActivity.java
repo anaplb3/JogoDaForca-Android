973a82f85b22664d15e7369bcdfffa9150d2f658
Emerson Dantas <emerson.ruan@dce.ufpb.br>
2019-01-19 21:22:22 -0300

    REQ04 - Teclado proprio

--- app/src/main/java/com/example/anaplb/appalpha/activities/ForcaActivity.java
+++ app/src/main/java/com/example/anaplb/appalpha/activities/ForcaActivity.java
@@ -6,6 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -20,6 +21,7 @@ import com.example.anaplb.appalpha.tratamento.TratandoPalavra;
 
 
 public class ForcaActivity extends AppCompatActivity {
+
     final int CHUTE_NULO = 2;
     final int CHUTE_VALIDO = 1;
     final int CHUTE_REPETIDO = -1;
@@ -73,15 +75,16 @@ public class ForcaActivity extends AppCompatActivity {
         ImageView imgPalavra = findViewById(R.id.img_palavra);
         imgPalavra.setImageResource(idImagem);
 
+        carregarTeclado();
+
     }
 
     /**
      * Atualiza os dados da tela como a img da forca e o TextView com o underscore
-     * @param v view
      */
-    public void atualizandoInformacoes(View v) {
+    public void atualizandoInformacoes(String letraClicada) {
         // Pegando a resposta e verificando se houve erro
-        pegandoResposta();
+        pegandoResposta(letraClicada);
 
         // Atualiza a imagem da forca de acordo com a quantidade de erros
         setandoAForca();
@@ -143,9 +146,8 @@ public class ForcaActivity extends AppCompatActivity {
     /**
      * Pega a resposta do usuário e verifica se houve acerto, resposta nulo ou repetida. A partir disso contabiliza a quantidade de erros
      */
-    public void pegandoResposta() {
-        EditText txt = findViewById(R.id.txt_chute);
-        chute = txt.getText().toString().toLowerCase();
+    public void pegandoResposta(String letraClicada) {
+        chute = letraClicada.toLowerCase();
 
         int res = tratandoPalavra.contandoErros(chute);
 
@@ -159,8 +161,6 @@ public class ForcaActivity extends AppCompatActivity {
             Toast.makeText(getApplicationContext(), "Você já chutou essa letra!", Toast.LENGTH_LONG).show();
 
         }
-
-        txt.getText().clear();
 
     }
 
@@ -180,6 +180,221 @@ public class ForcaActivity extends AppCompatActivity {
     private void startEmActivity(Intent it) {
         startActivity(it);
         finish();
+    }
+
+    /**
+     * Recupera os botões do teclado e os escuta para que a cada novo clique seja enviado a respectiva letra para ser testada
+     */
+    private void carregarTeclado(){
+        final Button a = findViewById(R.id.A);
+        final Button b = findViewById(R.id.B);
+        final Button c = findViewById(R.id.C);
+        final Button d = findViewById(R.id.D);
+        final Button e = findViewById(R.id.E);
+        final Button f = findViewById(R.id.F);
+        final Button g = findViewById(R.id.G);
+        final Button h = findViewById(R.id.H);
+        final Button i = findViewById(R.id.I);
+        final Button j = findViewById(R.id.J);
+        final Button k = findViewById(R.id.K);
+        final Button l = findViewById(R.id.L);
+        final Button m = findViewById(R.id.M);
+        final Button n = findViewById(R.id.N);
+        final Button o = findViewById(R.id.O);
+        final Button p = findViewById(R.id.P);
+        final Button q = findViewById(R.id.Q);
+        final Button r = findViewById(R.id.R);
+        final Button s = findViewById(R.id.S);
+        final Button t = findViewById(R.id.T);
+        final Button u = findViewById(R.id.U);
+        final Button v = findViewById(R.id.V);
+        final Button w = findViewById(R.id.W);
+        final Button x = findViewById(R.id.X);
+        final Button y = findViewById(R.id.Y);
+        final Button z = findViewById(R.id.Z);
+
+        a.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                atualizandoInformacoes("a");
+            }
+        });
+
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                atualizandoInformacoes("b");
+            }
+        });
+
+        c.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                atualizandoInformacoes("c");
+            }
+        });
+
+        d.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                atualizandoInformacoes("d");
+            }
+        });
+
+        e.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                atualizandoInformacoes("e");
+            }
+        });
+
+        f.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                atualizandoInformacoes("f");
+            }
+        });
+
+        g.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                atualizandoInformacoes("g");
+            }
+        });
+
+        h.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                atualizandoInformacoes("h");
+            }
+        });
+
+        i.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                atualizandoInformacoes("i");
+            }
+        });
+
+        j.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                atualizandoInformacoes("j");
+            }
+        });
+
+        k.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                atualizandoInformacoes("k");
+            }
+        });
+
+        l.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                atualizandoInformacoes("l");
+            }
+        });
+
+        m.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                atualizandoInformacoes("m");
+            }
+        });
+
+        n.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                atualizandoInformacoes("n");
+            }
+        });
+
+        o.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                atualizandoInformacoes("o");
+            }
+        });
+
+        p.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                atualizandoInformacoes("p");
+            }
+        });
+
+        q.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                atualizandoInformacoes("q");
+            }
+        });
+
+        r.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                atualizandoInformacoes("r");
+            }
+        });
+
+        s.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                atualizandoInformacoes("s");
+            }
+        });
+
+        t.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                atualizandoInformacoes("t");
+            }
+        });
+
+        u.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                atualizandoInformacoes("u");
+            }
+        });
+
+        v.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                atualizandoInformacoes("v");
+            }
+        });
+
+        w.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                atualizandoInformacoes("w");
+            }
+        });
+
+        x.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                atualizandoInformacoes("x");
+            }
+        });
+
+        y.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                atualizandoInformacoes("y");
+            }
+        });
+
+        z.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                atualizandoInformacoes("z");
+            }
+        });
+
     }
 
 }
