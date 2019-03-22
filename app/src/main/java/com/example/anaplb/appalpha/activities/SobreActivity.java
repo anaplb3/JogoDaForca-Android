3d33298da6a50b97ab59cf62812f91bd02245e2e
Emerson Dantas <emerson.ruan@dce.ufpb.br>
2019-03-22 04:23:06 -0300

    Creditos das imagens adicionados na tela "Sobre"

--- app/src/main/java/com/example/anaplb/appalpha/activities/SobreActivity.java
+++ app/src/main/java/com/example/anaplb/appalpha/activities/SobreActivity.java
@@ -1,6 +1,7 @@
 package com.example.anaplb.appalpha.activities;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -21,5 +22,10 @@ public class SobreActivity extends AppCompatActivity {
         Intent it = new Intent(getApplicationContext(), MainActivity.class);
         startActivity(it);
         finish();
+    }
+
+    public void redirecionarASite(View view){
+        Intent browserintent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://pt.freeimages.com"));
+        startActivity(browserintent);
     }
 }
