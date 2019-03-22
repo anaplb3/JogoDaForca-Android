f68b053052035a1bc666976b0b1ec237d4e9c429
Emerson Dantas <emerson.ruan@dce.ufpb.br>
2019-03-22 16:46:38 -0300

    Tela de configurações - WIP

---
+++ app/src/main/java/com/example/anaplb/appalpha/activities/ConfigActivity.java
@@ -0,0 +1,14 @@
+package com.example.anaplb.appalpha.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.example.anaplb.appalpha.R;
+
+public class ConfigActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_config);
+    }
+}
