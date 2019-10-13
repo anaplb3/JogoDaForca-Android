aa83b2f9107ae07fbcb62e03eb2d47faa53b3c89
emersondantas <emerson.ruan@dce.ufpb.br>
2019-10-13 04:17:23 -0300

    Adapter class of theme rv added

--- app/src/main/java/br/dcx/appalpha/view/activities/theme/ThemeAdapter.java
+++ app/src/main/java/br/dcx/appalpha/view/activities/theme/ThemeAdapter.java
@@ -11,10 +11,10 @@ import com.example.anaplb.appalpha.R;
 
 import java.util.List;
 
-import br.dcx.appalpha.model.bean.Challenge;
+import br.dcx.appalpha.model.bean.Theme;
 
 public class ThemeAdapter extends RecyclerView.Adapter<ThemeAdapter.ViewHolder> {
-        private List<Challenge> challenges;
+        private List<Theme> themes;
         private String  TAG = "ThemeListAdapter";
 
         public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
@@ -23,19 +23,28 @@ public class ThemeAdapter extends RecyclerView.Adapter<ThemeAdapter.ViewHolder> 
         }
 
         public int getItemCount(){
-                return challenges.size();
+                return themes.size();
         }
 
         public void onBindViewHolder(ViewHolder holder, int position) {
+                holder.themeNameLeft.setText(themes.get(position).getName());
+                if(position < themes.size()){
+                        holder.themeNameRight.setText(themes.get(position+1).getName());
+                }
         }
 
         class ViewHolder extends RecyclerView.ViewHolder{
-                ImageView themeImage;
-                TextView themeName;
+                ImageView themeImageLeft;
+                ImageView themeImageRight;
+                TextView themeNameLeft;
+                TextView themeNameRight;
 
                 public ViewHolder(View itemView) {
                         super(itemView);
-
+                        themeImageLeft = itemView.findViewById(R.id.img_left);
+                        themeImageRight = itemView.findViewById(R.id.img_right);
+                        themeNameLeft = itemView.findViewById(R.id.tv_theme_name_left);
+                        themeNameRight = itemView.findViewById(R.id.tv_theme_name_right);
                 }
 
         }
