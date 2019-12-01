40c040c9a5665f11abd7e44006dfa65899e7b5e3
emersondantas <emerson.ruan@dce.ufpb.br>
2019-12-01 13:40:59 -0300

    Before migrate to android x

---
+++ app/src/main/java/br/ufpb/dcx/appalpha/view/activities/AddThemeActivity.java
@@ -0,0 +1,16 @@
+package br.ufpb.dcx.appalpha.view.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import br.ufpb.dcx.appalpha.R;
+
+public class AddThemeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_theme);
+    }
+}
